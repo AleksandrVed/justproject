e6c258de44a58af7aea9201fc107c3a0f87a27b9
AleksandrVed <alexandervhrom@gmail.com>
2021-11-09 17:56:18 +0300

    12

--- bboard/api/urls.py
+++ bboard/api/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('bbs/<int:pk>/comments', views.comments),
     path('bbs/<int:pk>/', views.BbDetailView.as_view()),
     path('bbs/', views.bbs)
 ]
